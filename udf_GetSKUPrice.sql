cddb625291be50390a8a2cb93ad0b0ebb0a4d975
ViCoffeeSt <127383715+ViCoffeeSt@users.noreply.github.com>
2023-04-18 12:41:30 +0300

    Update udf_GetSKUPrice.sql

--- udf_GetSKUPrice.sql
+++ udf_GetSKUPrice.sql
@@ -1,12 +1,12 @@
-CREATE FUNCTION udf_GetSKUPrice (ID_SKU INTEGER)
+CREATE FUNCTION dbo.udf_GetSKUPrice (@ID_SKU INT)
 RETURNS DECIMAL(18,2)
 AS
 BEGIN
-  DECLARE Price DECIMAL(18,2);
-
-  SELECT Price = SUM(Value) / SUM(Quantity)
+  DECLARE @Price DECIMAL(18,2);
+  
+  SELECT @Price = SUM(Value) / SUM(Quantity)
   FROM Basket
-  WHERE ID_SKU = ID_SKU;
+  WHERE ID_SKU = @ID_SKU;
 
-  RETURN Price;
+  RETURN @Price;
 END;
